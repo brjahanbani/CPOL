c516056fd7baee4360f18a3c3b7e643f9f9bd62e
Babak <babak.jahanbani@yahoo.com>
2022-06-12 18:15:48 +0430

    initial global components

---
+++ src/app/layout/global/logo-type/logo-type.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoTypeComponent } from './logo-type.component';
+
+describe('LogoTypeComponent', () => {
+  let component: LogoTypeComponent;
+  let fixture: ComponentFixture<LogoTypeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoTypeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoTypeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/layout/global/logo-type/logo-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-type',
+  templateUrl: './logo-type.component.html',
+  styleUrls: ['./logo-type.component.css']
+})
+export class LogoTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
